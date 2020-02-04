8a569b18ecccdf9182bfb8078b8e24315332a021
DucklingTurtle <58099492+DucklingTurtle@users.noreply.github.com>
2020-02-04 14:55:58 -0800

    Update

--- forms.py
+++ forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, NumberRange
 
 class RegistrationFormStudent(FlaskForm):
-    name = StringField("Name", validators=[DataRequired(), Length(min=2, max=20)])
+    student_id = IntegerField("Student ID", validators=[DataRequired(), NumberRange(min=0, max=99999999)])
+    name = StringField("Last and First Name", validators=[DataRequired(), Length(min=2, max=20)])
+    focus1 = StringField("Field of Focus 1", validators=[DataRequired(), Length(min=2, max=40)])
+    focus2 = StringField("Field of Focus 2", validators=[DataRequired(), Length(min=2, max=40)])
     email = StringField("Email", validators=[DataRequired(), Email()])
     submit = SubmitField("Enter")
--- studentassignerblog.py
+++ studentassignerblog.py
@@ -1,32 +1,39 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationFormStudent
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "d2c3113ee16531331a94a16f82b0cd1e"
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
 
+
 @app.route("/students")
 def students():
     return render_template("students.html")
+
 
 @app.route("/teachers")
 def teachers():
     return render_template("teachers.html")
 
+
 @app.route("/admin")
 def admin():
     return render_template("administrator.html")
 
-@app.route("/student_register")
+
+@app.route("/student_register", methods=["GET", "POST"])
 def student_register():
     form = RegistrationFormStudent()
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!", "success")
-        return redirect(url_for("home"))
+
+        flash(f"Object created for {form.name.data}!", "success")
+        return redirect(url_for("students"))
     return render_template("register.html", title="Student Register", form=form)
 
 
