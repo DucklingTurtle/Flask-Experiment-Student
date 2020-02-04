ffc57a23dd5058be23c19b25b096b791d8363d2a
DucklingTurtle <58099492+DucklingTurtle@users.noreply.github.com>
2020-02-04 08:23:38 -0800

    Update

--- forms.py
+++ forms.py
@@ -1,2 +1,8 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
+class RegistrationFormStudent(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Enter")
--- studentassignerblog.py
+++ studentassignerblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationFormStudent
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "d2c3113ee16531331a94a16f82b0cd1e"
 
 @app.route("/")
 @app.route("/home")
@@ -17,6 +20,14 @@ def teachers():
 @app.route("/admin")
 def admin():
     return render_template("administrator.html")
+
+@app.route("/student_register")
+def student_register():
+    form = RegistrationFormStudent()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Student Register", form=form)
 
 
 if __name__ == '__main__':
