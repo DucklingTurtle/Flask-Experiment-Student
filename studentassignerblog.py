8072acb37b6253c00d1b2b334b6a37acd1c6e362
DucklingTurtle <58099492+DucklingTurtle@users.noreply.github.com>
2020-02-05 11:58:54 -0800

    Update

--- studentassignerblog.py
+++ studentassignerblog.py
@@ -1,5 +1,13 @@
+import gspread, sys
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationFormStudent
+from oauth2client.service_account import ServiceAccountCredentials
+
+# client google database
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('google-database.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open("Student_Register_Database").sheet1
 
 app = Flask(__name__)
 
@@ -30,8 +38,11 @@ def admin():
 @app.route("/student_register", methods=["GET", "POST"])
 def student_register():
     form = RegistrationFormStudent()
+    len_sheet = len(sheet.col_values(1))
+    logger.debug(len_sheet, file=sys.stder)
     if form.validate_on_submit():
-
+        len_sheet = len(sheet.col_values(1))
+        print(len_sheet)
         flash(f"Object created for {form.name.data}!", "success")
         return redirect(url_for("students"))
     return render_template("register.html", title="Student Register", form=form)
