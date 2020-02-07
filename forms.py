712e60159dddad5688c46772e09a69c438635893
DucklingTurtle <58099492+DucklingTurtle@users.noreply.github.com>
2020-02-06 16:06:12 -0800

    Register forms working

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Length, Email, NumberRange
 
+
 class RegistrationFormStudent(FlaskForm):
     student_id = IntegerField("Student ID", validators=[NumberRange(min=0, max=99999999, message="Error: Entry must be a number")])
     name = StringField("Last and First Name", validators=[DataRequired(), Length(min=2, max=20)])
@@ -10,5 +11,13 @@ class RegistrationFormStudent(FlaskForm):
     focus2 = SelectField("Focus 2", choices=[("AP", "American Politics"), ("CP", "Comparative Politics"),
                                              ("IR", "International Relations"), ("MT", "Methodology"),
                                              ("TH", "Political Theory")])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Enter")
+
+
+class RegistrationFormTeacher(FlaskForm):
+    name = StringField("Last and First Name", validators=[DataRequired(), Length(min=2, max=20)])
+    focus = SelectField("Focus", choices=[("AP", "American Politics"), ("CP", "Comparative Politics"),
+                                             ("IR", "International Relations"), ("MT", "Methodology"), ("TH", "Political Theory")])
     email = StringField("Email", validators=[DataRequired(), Email()])
     submit = SubmitField("Enter")
--- studentassignerblog.py
+++ studentassignerblog.py
@@ -1,15 +1,17 @@
-import gspread, sys, Student_Register_Sheet
+import Teacher_Register_Sheet, Student_Register_Sheet, os
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationFormStudent
-from oauth2client.service_account import ServiceAccountCredentials
+from forms import RegistrationFormStudent, RegistrationFormTeacher
+
+os.path.join(os.path.curdir, 'google-database.json')
 
 # google sheets
-# s_r = Student_Register_Sheet.worksheet
 s_r = Student_Register_Sheet.client.open("Student_Register_Database").sheet1
+t_r = Teacher_Register_Sheet.client.open("Teacher_Register_Database").sheet1
+
 
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = "d2c3113ee16531331a94a16f82b0cd1e"
+
 
 def create_student(student_id, name, focus1, focus2, email):
     len_sheet = len(s_r.col_values(1))
@@ -19,6 +21,15 @@ def create_student(student_id, name, focus1, focus2, email):
     s_r.update_cell(cell, 3, focus1)
     s_r.update_cell(cell, 4, focus2)
     s_r.update_cell(cell, 5, email)
+
+
+def create_teacher(name, focus, email):
+    len_sheet = len(t_r.col_values(1))
+    cell = len_sheet + 1
+    t_r.update_cell(cell, 1, name)
+    t_r.update_cell(cell, 2, focus)
+    t_r.update_cell(cell, 3, email)
+
 
 @app.route("/")
 @app.route("/home")
@@ -52,16 +63,22 @@ def student_register():
         focus2 = form.focus2.data
         email = form.email.data
         create_student(student_id, name, focus1, focus2, email)
-        # len_sheet = len(s_r.col_values(1))
-        # cell = len_sheet + 1
-        # s_r.update_cell(cell, 1, form.student_id.data)
-        # s_r.update_cell(cell, 2, form.name.data)
-        # s_r.update_cell(cell, 3, form.focus1.data)
-        # s_r.update_cell(cell, 4, form.focus2.data)
-        # s_r.update_cell(cell, 5, form.email.data)
         flash(f"Database entry created for {form.name.data}!", "success")
         return redirect(url_for("students"))
     return render_template("register.html", title="Student Register", form=form)
+
+
+@app.route("/teacher_register", methods=["GET", "POST"])
+def teacher_register():
+    form = RegistrationFormTeacher()
+    if form.validate_on_submit():
+        name = form.name.data
+        focus = form.focus.data
+        email = form.email.data
+        create_teacher(name, focus, email)
+        flash(f"Database entry created for {form.name.data}!", "success")
+        return redirect(url_for("teachers"))
+    return render_template("register_t.html", title="Teacher Register", form=form)
 
 
 if __name__ == '__main__':
