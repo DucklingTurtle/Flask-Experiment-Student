873a180f9bf598a571466deef5a7db3dffe0e56c
DucklingTurtle <58099492+DucklingTurtle@users.noreply.github.com>
2020-03-13 06:48:46 -0700

    update

--- forms.py
+++ forms.py
@@ -4,7 +4,6 @@ from wtforms.validators import DataRequired, Length, Email, NumberRange
 
 
 class RegistrationFormStudent(FlaskForm):
-    student_id = IntegerField("Student ID", validators=[NumberRange(min=0, max=99999999, message="Error: Entry must be a number")])
     name = StringField("Last and First Name", validators=[DataRequired(), Length(min=2, max=20)])
     focus1 = SelectField("Focus 1", choices=[("AP", "American Politics"), ("CP", "Comparative Politics"),
                                              ("IR", "International Relations"), ("MT", "Methodology"), ("TH", "Political Theory")])
@@ -17,7 +16,11 @@ class RegistrationFormStudent(FlaskForm):
 
 class RegistrationFormTeacher(FlaskForm):
     name = StringField("Last and First Name", validators=[DataRequired(), Length(min=2, max=20)])
-    focus = SelectField("Focus", choices=[("AP", "American Politics"), ("CP", "Comparative Politics"),
+    focus1 = SelectField("Focus 1", choices=[("AP", "American Politics"), ("CP", "Comparative Politics"),
                                              ("IR", "International Relations"), ("MT", "Methodology"), ("TH", "Political Theory")])
+    focus2 = SelectField("Focus 2", choices=[("AP", "American Politics"), ("CP", "Comparative Politics"),
+                                           ("IR", "International Relations"), ("MT", "Methodology"),
+                                           ("TH", "Political Theory")])
+
     email = StringField("Email", validators=[DataRequired(), Email()])
     submit = SubmitField("Enter")
--- studentassignerblog.py
+++ studentassignerblog.py
@@ -1,34 +1,69 @@
-import Teacher_Register_Sheet, Student_Register_Sheet
-from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationFormStudent, RegistrationFormTeacher
-
-# google sheets
-try:
-    s_r = Student_Register_Sheet.client.open("Student_Register_Database").sheet1
-    t_r = Teacher_Register_Sheet.client.open("Teacher_Register_Database").sheet1
-except:
-    flash("Error importing database")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "d2c3113ee16531331a94a16f82b0cd1e"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
+# database models
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    focus1 = db.Column(db.String(30), nullable=False)
+    focus2 = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(70), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Student('{self.name}', '{self.focus1}', '{self.focus2}', '{self.email}')"
 
 
-def create_student(student_id, name, focus1, focus2, email):
-    len_sheet = len(s_r.col_values(1))
-    cell = len_sheet + 1
-    s_r.update_cell(cell, 1, student_id)
-    s_r.update_cell(cell, 2, name)
-    s_r.update_cell(cell, 3, focus1)
-    s_r.update_cell(cell, 4, focus2)
-    s_r.update_cell(cell, 5, email)
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    focus1 = db.Column(db.String(30), nullable=False)
+    focus2 = db.Column(db.String(30), nullable=False)
+    days_avail = db.Column(db.String(40), nullable=False)
+    hours = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(70), nullable=False)
+    students = db.Column(db.String(40))
+    day1 = db.Column(db.String(40))
+    day2 = db.Column(db.String(40))
+    day3 = db.Column(db.String(40))
+    day4 = db.Column(db.String(40))
+    day5 = db.Column(db.String(40))
+    day6 = db.Column(db.String(40))
+    day7 = db.Column(db.String(40))
+    day8 = db.Column(db.String(40))
+    day9 = db.Column(db.String(40))
+    day10 = db.Column(db.String(40))
+    hours_taken = db.Column(db.String(40))
+    first_pass = db.Column(db.String(40))
 
 
-def create_teacher(name, focus, email):
-    len_sheet = len(t_r.col_values(1))
-    cell = len_sheet + 1
-    t_r.update_cell(cell, 1, name)
-    t_r.update_cell(cell, 2, focus)
-    t_r.update_cell(cell, 3, email)
+# create students in database
+def create_student(name, focus1, focus2, email):
+    student = Student(name=name, focus1=focus1, focus2=focus2, email=email)
+    db.session.add(student)
+    db.session.commit()
+
+# create teachers in database
+def create_teacher(name, focus1, focus2, days_avail, hours, email):
+    teacher = Teacher(name=name, focus1=focus1, focus2=focus2, days_avail=days_avail, hours=hours, email=email)
+    db.session.add(teacher)
+    db.session.commit()
+
+
+# get list of students
+def get_students_list():
+    students_list = Student.query.all()
+    return students_list
+
+# get list of teachers
+def get_teachers_list():
+    teachers_list = Teacher.query.all()
+    return teachers_list
 
 
 @app.route("/")
@@ -51,18 +86,25 @@ def teachers():
 def admin():
     return render_template("administrator.html")
 
+@app.route("/admin_students")
+def admin_students():
+    students_list = get_students_list()
+    return render_template("admin_students.html", students_list=students_list)
+
+@app.route("/admin_teachers")
+def admin_teachers():
+    teachers_list = get_teachers_list()
+    return render_template("admin_teachers.html", teachers_list=teachers_list)
 
 @app.route("/student_register", methods=["GET", "POST"])
 def student_register():
     form = RegistrationFormStudent()
-    # print(len_sheet, file=sys.stderr)
     if form.validate_on_submit():
-        student_id = form.student_id.data
         name = form.name.data
         focus1 = form.focus1.data
         focus2 = form.focus2.data
         email = form.email.data
-        create_student(student_id, name, focus1, focus2, email)
+        create_student(name, focus1, focus2, email)
         flash(f"Database entry created for {form.name.data}!", "success")
         return redirect(url_for("students"))
     return render_template("register.html", title="Student Register", form=form)
@@ -72,12 +114,16 @@ def student_register():
 def teacher_register():
     form = RegistrationFormTeacher()
     if form.validate_on_submit():
-        name = form.name.data
-        focus = form.focus.data
-        email = form.email.data
-        create_teacher(name, focus, email)
-        flash(f"Database entry created for {form.name.data}!", "success")
-        return redirect(url_for("teachers"))
+        if request.method == "POST":
+            name = form.name.data
+            focus1 = form.focus1.data
+            focus2 = form.focus2.data
+            days = str(request.form.getlist("days_checkbox"))
+            hours = str(request.form.getlist("hours_checkbox"))
+            email = form.email.data
+            create_teacher(name, focus1, focus2, days, hours, email)
+            flash(f"Database entry created for {form.name.data}!", "success")
+            return redirect(url_for("teachers"))
     return render_template("register_t.html", title="Teacher Register", form=form)
 
 
